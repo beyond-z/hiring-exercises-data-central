8ad7bea40bd05c8c3abd41d0c5e7d77129ce740e
Jaime Bellmyer <online@bellmyer.com>
2018-11-23 10:42:38 -0600

    Cherry-pick b2dbb4dafa66988bcdd885af00c56b0d3df0a83d 'Add emails' from real data-central

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_140407) do
+ActiveRecord::Schema.define(version: 2018_11_23_155501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "emails", force: :cascade do |t|
+    t.string "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["value"], name: "index_emails_on_value", unique: true
+  end
 
   create_table "industries", force: :cascade do |t|
     t.string "name"
